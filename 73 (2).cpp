b4e2e2f7e0da3c2823176b453571b6da302769fe
Banegas , Agustina <133693422+BanegasAgustina@users.noreply.github.com>
2023-10-02 19:20:52 -0300

    Add files via upload

---
+++ 73 (2).cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> SumarVectores(vector<int> A,vector<int> B) {
+    vector<int> C;
+    for(int i = 0; i < A.size(); ++i) {
+        C.push_back(A[i] + B[i]);
+    }
+    return C;
+}
+
+void imprimir(vector<int> vec) {
+    for(int i = 0; i < vec.size(); ++i) {
+        cout << vec[i] << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    srand(time(0));
+   
+    vector<int> A(10), B(10);
+
+    // Llenar los vectores con números aleatorios menores a 1500
+    for(int i = 0; i < 10; ++i) {
+        A[i] = rand() % 1500;
+        B[i] = rand() % 1500;
+    }
+   
+    vector<int> C = SumarVectores(A, B);
+   
+    // Mostrar los vectores
+    cout << "Vector A: ";
+    imprimir(A);
+    cout << "Vector B: ";
+    imprimir(B);
+    cout << "Vector C: ";
+    imprimir(C);
+
+    return 0;
+}
+
+
+
